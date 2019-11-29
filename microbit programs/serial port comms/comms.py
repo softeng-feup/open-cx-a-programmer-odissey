673215fa5c6c9b12a3e25c1c500ec0e8f77d1423
Gonçalo Oliveira <goncalo.oli@outlook.com>
2019-11-29 09:45:02 +0000

    Fixed style issues

--- microbit programs/serial port comms/comms.py
+++ microbit programs/serial port comms/comms.py
@@ -3,34 +3,32 @@
 import serial
 import serial.tools.list_ports
 
-def getMicrobitPort():
 
+def get_microbit_port():
     serial_ports = list(serial.tools.list_ports.comports())
 
     for port in serial_ports:
         sep = str(port.description).find(' (')
         port_description = str(port.description)[:sep]
         if port_description == 'mbed Serial Port':
-            return port.device;
+            return port.device
 
-def writeFileToFile (port,filename) :
 
-    mb_port = serial.Serial(port,19200)
-    f = open(filename,"r")
+def write_file_to_file(port, filename):
+    mb_port = serial.Serial(port, 19200)
+    f = open(filename, "r")
 
     for line in f:
         print(line)
-        mb_port.write(line),
+        mb_port.write(line)
 
     f.close()
     mb_port.close()
-    
-    return;
 
-def readFileToFile (port,filename) :
 
-    mb_port = serial.Serial(port,19200)
-    f = open(filename,"w")
+def read_file_to_file(port, filename):
+    mb_port = serial.Serial(port, 19200)
+    f = open(filename, "w")
     line = mb_port.readline()
 
     while line:
@@ -39,57 +37,44 @@ def readFileToFile (port,filename) :
 
     f.close()
     mb_port.close()
-    
-    return;
 
-def writeConsoleToConsole (port) :
 
-    mb_port = serial.Serial(port,19200,timeout=20)
+def write_console_to_console(port):
+    mb_port = serial.Serial(port, 19200, timeout=20)
     i = input()
 
     mb_port.write(str.encode(i))
 
     mb_port.close()
-    
-    return;
 
-def readConsoleToConsole(port):
 
-    mb_port = serial.Serial(port,19200,timeout=20)
+def read_console_to_console(port):
+    mb_port = serial.Serial(port, 19200, timeout=20)
     i = mb_port.read(10)
 
     print(i)
 
     mb_port.close()
 
-    return;
 
-def readConsoleToFile(port,filename):
-    
-    mb_port = serial.Serial(port,19200,timeout=20)
+def read_console_to_file(port, filename):
+    mb_port = serial.Serial(port, 19200, timeout=20)
     i = mb_port.read(10)
 
-    f = open(filename,"w")
+    f = open(filename, "w")
     f.write(str(i))
     f.close()
 
     mb_port.close()
-    
-    return;
 
-microbit_port = getMicrobitPort()
+
+microbit_port = get_microbit_port()
 
 print("Send(S) or Receive(R):")
 i = input()
 if i == 'S':
-    writeConsoleToConsole('COM1')
+    write_console_to_console('COM3')
 elif i == 'R':
-    readConsoleToFile('COM3','text.txt')
-else : print("Invalid input!")
-
-
-
-
-
-
-
+    read_console_to_file('COM3', 'text.txt')
+else:
+    print("Invalid input!")
